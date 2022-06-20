f9819fd630f005cc10eebde2579189b2f1339bf6
tijDrame <tijdrame@gmail.com>
2022-06-20 11:55:14 +0100

    finis

--- src/main/java/com/emard/api/restfulwebservice/exception/ExceptionResponse.java
+++ src/main/java/com/emard/api/restfulwebservice/exception/ExceptionResponse.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 public class ExceptionResponse {
     private LocalDate timeStamp;
     private String message;
